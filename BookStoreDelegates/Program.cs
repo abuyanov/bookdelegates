8061e3bc9d06d0b1cc815cfd79189c0d112bd50e
Aleksandr Buyanov <abuyan78@yandex.ru>
2018-09-11 23:00:59 +0300

    Добавьте файлы проекта.

---
+++ BookStoreDelegates/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreDelegates
+{
+    using Bookstore;
+
+    class Program
+    {
+        // Print the title of the book.
+        static void PrintTitle(Book b)
+        {
+            System.Console.WriteLine("   {0}", b.Title);
+        }
+
+        // Initialize the book database with some test books:
+        static void AddBooks(BookDB bookDB)
+        {
+            bookDB.AddBook("The C Programming Language", "Brian W. Kernighan and Dennis M. Ritchie", 19.95m, true);
+            bookDB.AddBook("The Unicode Standard 2.0", "The Unicode Consortium", 39.95m, true);
+            bookDB.AddBook("The MS-DOS Encyclopedia", "Ray Duncan", 129.95m, false);
+            bookDB.AddBook("Dogbert's Clues for the Clueless", "Scott Adams", 12.00m, true);
+        }
+
+        static void Main(string[] args)
+        {
+            BookDB bookDB = new BookDB();
+            AddBooks(bookDB);
+            // Print all the titles of paperbacks:
+            System.Console.WriteLine("Paperback Book Titles:");
+
+            // Create a new delegate object associated with the static 
+            // method Test.PrintTitle:
+            bookDB.ProcessPaperbackBooks(PrintTitle);
+
+            // Get the average price of a paperback by using
+            // a PriceTotaller object:
+            PriceTotaller totaller = new PriceTotaller();
+
+            // Create a new delegate object associated with the nonstatic 
+            // method AddBookToTotal on the object totaller:
+            bookDB.ProcessPaperbackBooks(totaller.AddBookToTotal);
+
+            System.Console.WriteLine("Average Paperback Book Price: ${0:#.##}",
+                    totaller.AveragePrice());
+
+            System.Console.ReadLine();
+        }
+    }
+
+    // Class to total and average prices of books:
+    class PriceTotaller
+    {
+        int countBooks = 0;
+        decimal priceBooks = 0.0m;
+
+        internal void AddBookToTotal(Book book)
+        {
+            countBooks += 1;
+            priceBooks += book.Price;
+        }
+
+        internal decimal AveragePrice()
+        {
+            return priceBooks / countBooks;
+        }
+    }
+
+}
